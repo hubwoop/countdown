8910033b2919b7e8b4adc2a03d510d2b9f58c68f
Jonas Mai <jo@jonasmai.de>
2018-02-27 18:15:13 +0100

    fancy sun animation refactored js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ function Location(city, latitude, longitude, timeZoneOffset) {
     this.latitude = latitude;
     this.longitude = longitude;
     this.timeZoneOffset = timeZoneOffset;
+    this.dayLength = 0;
 }
 
 function Gradient(top, bottom) {
@@ -38,19 +39,19 @@ function Half(location, id, hasParticles) {
 
 const countdownDate = new Date(Date.UTC(2018, 7, 2, 19, 35)).getTime();
 
-let melbourne = new Location("melbourne", -37.814, 144.96332, 11);
-let erlangen = new Location("erlangen", 49.59099, 11.00783, 1);
-let halves = new Halves(
-    new Half(erlangen, 'upperhalf', false),
-    new Half(melbourne, 'lowerhalf', false)
-);
-
 const daytimeGradients = {
     dawn: new Gradient('#63adf7', '#ffb539'),
     dusk: new Gradient('#485661', '#ff822b'),
     night: new Gradient('#0a1722', '#415a84'),
     day: new Gradient('#86d4f7', '#55a7ff')
 };
+
+let melbourne = new Location("melbourne", -37.814, 144.96332, 11);
+let erlangen = new Location("erlangen", 49.59099, 11.00783, 1);
+let halves = new Halves(
+    new Half(erlangen, 'upperhalf', false),
+    new Half(melbourne, 'lowerhalf', false)
+);
 
 function updateCountdown(date) {
 
@@ -71,51 +72,44 @@ function updateCountdown(date) {
 }
 
 function updateLocalTimes(date) {
-
     let hourErlangen = (date.getUTCHours() + 1) % 24;
     let hourMelbourne = (date.getUTCHours() + 11) % 24;
     let minute = forceTwoDigits(date.getMinutes());
     let second = forceTwoDigits(date.getSeconds());
     document.getElementById("erlangen").innerHTML = hourErlangen + ":" + minute + ":" + second;
     document.getElementById("melbourne").innerHTML = hourMelbourne + ":" + minute + ":" + second;
+}
 
-    function setCityGradient(city, gradient) {
-        document.body.style.setProperty(`--${city}-top-color`, gradient.top);
-        document.body.style.setProperty(`--${city}-bottom-color`, gradient.bottom);
-    }
-
-    function decideOnGradient(location) {
-        // console.log(`${date} >= ${location.sunrise} && ${date} <= ${location.sunset}`);
-        let gradient;
-        if (date >= location.twilightBegin && date <= location.twilightEnd) {
-
-            if (date <= location.sunrise) {
-                gradient = daytimeGradients.dawn;
-            } else if (date <= location.sunset) {
-                gradient = daytimeGradients.day;
-            } else {
-                gradient = daytimeGradients.dusk;
-            }
+function decideOnGradient(location, date) {
+    // console.log(`${date} >= ${location.sunrise} && ${date} <= ${location.sunset}`);
+    if (date >= location.twilightBegin && date <= location.twilightEnd) {
+        if (date <= location.sunrise) {
+            return daytimeGradients.dawn;
+        } else if (date <= location.sunset) {
+            return daytimeGradients.day;
         } else {
-            gradient = daytimeGradients.night;
+            return daytimeGradients.dusk;
         }
-        setCityGradient(location.city, gradient);
-
-        let half = (location === halves.upper.location) ? halves.upper: halves.lower;
-        if(gradient === daytimeGradients.night && !half.hasParticles) {
-            particlesJS.load(half.id, 'js/assets/particles.json');
-            half.hasParticles = true;
-        }
-        if (gradient !== daytimeGradients.night && half.hasParticles) {
-            half.element.removeChild(half.element.firstChild);
-            half.hasParticles = false;
-        }
-
+    } else {
+        return daytimeGradients.night;
     }
+}
 
-    decideOnGradient(erlangen);
-    decideOnGradient(melbourne);
+function setCityGradient(city, gradient) {
+    document.body.style.setProperty(`--${city}-top-color`, gradient.top);
+    document.body.style.setProperty(`--${city}-bottom-color`, gradient.bottom);
+}
 
+function nightModeTriggers(location, gradient) {
+    let half = (location === halves.upper.location) ? halves.upper : halves.lower;
+    if (gradient === daytimeGradients.night && !half.hasParticles) {
+        particlesJS.load(half.id, 'js/assets/particles.json');
+        half.hasParticles = true;
+    }
+    if (gradient !== daytimeGradients.night && half.hasParticles) {
+        half.element.removeChild(half.element.firstChild);
+        half.hasParticles = false;
+    }
 }
 
 // 1 second interval for countdown & clocks
@@ -124,6 +118,14 @@ let x = setInterval(function () {
     let date = new Date();
     updateCountdown(date);
     updateLocalTimes(date);
+
+    let gradient = decideOnGradient(erlangen, date);
+    setCityGradient(erlangen.city, gradient);
+    nightModeTriggers(erlangen, gradient);
+
+    gradient = decideOnGradient(melbourne, date);
+    setCityGradient(melbourne.city, gradient);
+    nightModeTriggers(melbourne, gradient);
 
 }, 1000);
 
@@ -134,13 +136,19 @@ function forceTwoDigits(i) {
 function getSunTimes(location) {
     // Melbourne https://api.sunrise-sunset.org/json?lat=-37.814&lng=-144.96332
     // Erlangen https://api.sunrise-sunset.org/json?lat=49.59099&lng=11.00783
-    let fetchDate = new Date();
-    if((fetchDate.getUTCHours() + location.timeZoneOffset) >= 24) {
-        fetchDate = 'tomorrow';
+
+    function decideOnFetchDate() {
+        let localDate = new Date();
+        if ((localDate.getUTCHours() + location.timeZoneOffset) >= 24) {
+            return 'tomorrow';
+        } else if ((localDate.getUTCHours() + location.timeZoneOffset) < 0) {
+            return 'yesterday';
+        } else {
+            return 'today';
+        }
     }
-    else {
-        fetchDate = 'today';
-    }
+
+    let fetchDate = decideOnFetchDate();
 
     fetch(`https://api.sunrise-sunset.org/json?lat=${location.latitude}&lng=${location.longitude}&formatted=0&date=${fetchDate}`)
         .then(function (response) {
@@ -154,6 +162,7 @@ function getSunTimes(location) {
                 location.sunset = new Date(data.results.sunset);
                 location.twilightBegin = new Date(data.results.civil_twilight_begin);
                 location.twilightEnd = new Date(data.results.civil_twilight_end);
+                location.dayLength = data.results.daylength;
             });
         })
         .catch(function (err) {
